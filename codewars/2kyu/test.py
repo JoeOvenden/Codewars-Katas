3e8abcea1201fe2fda92665c737c6a3be1560294
Joe Ovenden <joe.ovenden17@gmail.com>
2024-03-10 17:58:31 +0700

    Codewars katas

--- codewars/2kyu/test.py
+++ codewars/2kyu/test.py
@@ -2,7 +2,7 @@ import itertools
 
 if __name__ == "__main__":
     n = 3
-    products = itertools.product("BRG", repeat=n)
+    products = itertools.product("BRGye", repeat=n)
     products = ["".join(prod) for prod in products]
     print(products)
     print(len(products))
--- codewars/4kyu/snail-sort.py
+++ codewars/4kyu/snail-sort.py
@@ -12,19 +12,48 @@
 #          [7,6,5]]
 # snail(array) #=> [1,2,3,4,5,6,7,8,9]
 
-def next_direction(dir):
-    if dir == [1, 0]:
+# https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/python
+
+def next_direction(direction):
+    if direction == [1, 0]:
         return [0, 1]
-    if dir == [0, 1]:
+    if direction == [0, 1]:
         return [-1, 0]
-    if dir == [-1, 0]:
+    if direction == [-1, 0]:
         return [0, -1]
-    if dir == [0, -1]:
+    if direction == [0, -1]:
         return [1, 0]
 
+
+def move(pos, direction, multiplier=1):
+    for i in range(2):
+        pos[i] += direction[i] * multiplier
+
+
+def pos_out_of_bounds(snail_map, pos):
+    if pos[0] < 0 or pos[0] >= len(snail_map) or pos[1] < 0 or pos[1] >= len(snail_map[0]):
+        return True
+
+
 def snail(snail_map):
-    direction = [1, 0]  # [x, y]
+    direction = [0, 1]
     arr = []
-    x = 0, y = 0
-    while True:
-        pass
+    pos = [0, 0]
+    remaining = len(snail_map) * len(snail_map[0])
+    while remaining > 0:
+        if pos_out_of_bounds(snail_map, pos) or snail_map[pos[0]][pos[1]] == None:
+            move(pos, direction, multiplier=-1)
+            direction = next_direction(direction)
+            move(pos, direction)
+            continue
+        arr.append(snail_map[pos[0]][pos[1]])
+        snail_map[pos[0]][pos[1]] = None
+        remaining -= 1
+        move(pos, direction)
+    return arr
+
+if __name__ == "__main__":
+    arr = [[1, 2, 3],
+           [8, 9, 4],
+           [7, 6, 5]]
+    print(snail(arr))
